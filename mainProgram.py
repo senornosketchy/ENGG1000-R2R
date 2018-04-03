dee7a02396a8e485c17a25173c496ce9f1d6bf54
tanvee.islam <tanvee.islam@DETNSW>
2018-04-03 18:50:25 +1000

    Moved the subroutines from searchAndDestroy.py, lostandchargeahead.py and startSequence.py into a draft mainProgram.py

---
+++ mainProgram.py
@@ -0,0 +1,67 @@
+from time import sleep
+import sys, os
+
+# Import the ev3dev specific library
+from ev3dev.ev3 import *
+
+# Connect motors
+rightMotor = LargeMotor(OUTPUT_C)
+
+assert rightMotor.connected
+
+leftMotor = LargeMotor(OUTPUT_B)
+assert leftMotor.connected
+
+# Connect sensors
+
+us = UltrasonicSensor()
+
+cs = ColorSensor()
+
+gs = GyroSensor()
+
+gs.mode = 'GYRO-ANG'  # Set gyro mode to return compass angle
+
+# Declaring buttons
+btn = Button()
+
+
+# Basic movement control
+def drive(left, right):
+    """
+    Start both motors at the given speeds.
+    """
+    leftMotor.run_direct(duty_cycle_sp=left)
+
+    rightMotor.run_direct(duty_cycle_sp=right)
+
+
+# Spinning the robot, clockwise/anticlockwise depending on required direction
+def search(spinDirection):
+    drive(spinDirection * -100, spinDirection * 100)
+
+
+# Stop both motors
+def stop():
+    leftMotor.stop(stop_action='brake')
+
+    rightMotor.stop(stop_action='brake')
+
+# Basic Start sequence
+def startSequence(spinDirection):
+    sleep(3)
+    while gs.value() < 150:
+        search(spinDirection)
+        if us.value < 750:
+            drive(100, 100)
+        else:
+            search(spinDirection)
+
+# If the robot cannot see the other bot after the starting sequence
+def lost():
+    # If robot cannot find object drive forward to boundary then do another check
+
+    # Below loop, keeps the robot driving back and forth till target is found.
+    while cs.value() > 30:
+        drive(200, 200)
+    # Didn't know the code, to make it spin 180 degrees.
