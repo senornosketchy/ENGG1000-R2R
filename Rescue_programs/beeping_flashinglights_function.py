ecbcb6d02a48153c5a91481907ff1a8810682c45
aarora2000 <37999494+aarora2000@users.noreply.github.com>
2018-04-30 14:39:10 +1000

    Update beeping_flashinglights_function.py
    Fixing indenting.

--- Rescue_programs/beeping_flashinglights_function.py
+++ Rescue_programs/beeping_flashinglights_function.py
@@ -2,7 +2,7 @@ def beepingflashing():
         import time
         while not btn.any()
             Sound.tone([(1000, 500, 500)] * 20)
-        	Leds.set_color(Leds.RIGHT, Leds.RED)
+            Leds.set_color(Leds.RIGHT, Leds.RED)
             Leds.set_color(Leds.LEFT, Leds.RED)
             time.sleep(1)
             Leds.set_color(Leds.RIGHT, Leds.GREEN)
