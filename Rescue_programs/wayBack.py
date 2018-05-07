bbafbf376798d36c0c7832450641070dcd537dda
aarora2000 <37999494+aarora2000@users.noreply.github.com>
2018-05-07 15:39:27 +1000

    Update wayBack.py

--- Rescue_programs/wayBack.py
+++ Rescue_programs/wayBack.py
@@ -2,19 +2,24 @@ from ev3.lego import Motor
 c = Motor(port = Motor.PORT.C)
 from arrays import *
 
-initialMotorLocation= c.read_value("position")
+while btn.any():
+    
+    initialMotorLocation= c.read_value("position")
+    #while can not found    
+        #code in choosing the direction
+        if direction == forward:
+            wayBack[movementNumber] = forward();
+        else if direction == left:
+            wayBack[movementNumber] = turn(right);
+        else if direction == right:
+            wayBack[movementNumber] = turn(left);
+        #code in moving the bot    
 
-def wayBack(direction,movementNumber):
-    if direction == forward:
-        wayBack[movementNumber] = forward();
-    else if direction == left:
-        wayBack[movementNumber] = turn(right);
-    else if direction == right:
-        wayBack[movementNumber] = turn(left);
-
-
-def headingback(movementNumber):
+def headingback(movementNumber,initialMotorLocation,wayBack[]):
+    turn(left);
+    turn(left);
     i = movementNumber;
     while c.read_value("position") != initialMotorLocation or i != 0:
         wayBack[i];
         i = i - 1;
+
