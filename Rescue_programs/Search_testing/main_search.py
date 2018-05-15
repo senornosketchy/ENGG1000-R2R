6c8264b47681295623c7afdc57bfdd594cd64513
tanvee.islam <tanvee.islam@DETNSW>
2018-05-15 11:54:44 +1000

    Added some print statements for easier debugging

--- Rescue_programs/Search_testing/main_search.py
+++ Rescue_programs/Search_testing/main_search.py
@@ -117,51 +117,50 @@ def move_1_block_2(forward):
             i += 1
     stop_motors()
 
+
+def move_1_block_3():
+    # TODO: Figure out how to do desired direction
+    desired_direction = gs.value()
+
+    global i
+
+    a = -1
+    ultrasonic_movement(180)
+    sleep(3)
+    leftMotor.run_direct(duty_cycle_sp=a*75)
+    rightMotor.run_direct(duty_cycle_sp=a*75)
+
+    left_running_state = leftMotor.state
+    right_running_state = rightMotor.state
+    i = 0
+    while i < 67:
+        print("while loop count:", i)
+        print("Front US value", us_front.value())
+        if us.value() < 150:
+            stop_motors()
+            print()
+            print("wall was sensed early so motor stopped")
+            break
+        elif gs.value() < desired_direction - 3:
+            leftMotor.run_direct(duty_cycle_sp=a*30)
+            rightMotor.run_direct(duty_cycle_sp=a*75)
+            i += 1
+        elif gs.value() > desired_direction + 3:
+            leftMotor.run_direct(duty_cycle_sp=a*75)
+            rightMotor.run_direct(duty_cycle_sp=a*30)
+            i += 1
+        else:
+            leftMotor.run_direct(duty_cycle_sp=a*75)
+            rightMotor.run_direct(duty_cycle_sp=a*75)
+            i += 1
+    stop_motors()
+
 # this function stops both motors
 def stop_motors():
     # leftMotor.reset()
     leftMotor.stop()
     # rightMotor.reset()
     rightMotor.stop()
-
-
-def reverse():
-    """
-    Write the damn docstring
-    :return:
-    """
-    leftMotor.run_timed(time_sp=2000, speed_sp=-500)
-    rightMotor.run_timed(time_sp=2000, speed_sp=-500)
-
-
-def turn(target_angle, direction):
-    """
-    AIM: TO TURN THE ROBOT 90 DEGREES CLOCKWISE OR COUNTERCLOCKWISE 90 DEGREES
-
-    :param direction: Either a 1 for right turns or -1 for left turns
-
-    :return: NO RETURN
-    """
-    init_angle = gs.value() % 90
-    print("The inital angle:", init_angle)
-
-    print("The initial difference is:", init_angle - target_angle)
-
-    # TODO: Experiment to find the right speed and time values to turn 90 degrees
-    while abs(init_angle - target_angle) > 5:
-        print("The difference angle is now:", abs(init_angle - target_angle))
-
-        leftMotor.run_direct(duty_cycle_sp=direction * 50)
-        rightMotor.run_direct(duty_cycle_sp=direction * -50)
-
-        init_angle = gs.value() % 90
-
-    # while gs.value() % 360 != init_angle + direction*(target_angle - 1) or gs.value() % 360 != init_angle + \
-    #         direction*target_angle or gs.value() % 360 != init_angle + direction*(target_angle + 1):
-    #     print(gs.value() % 360)
-    #     leftMotor.run_direct(duty_cycle_sp=direction*60)
-    #     rightMotor.run_direct(duty_cycle_sp=direction*-60)
-    print("The final angle is:", init_angle)
 
 
 def gsturn(left):
@@ -206,7 +205,7 @@ def gsturn(left):
 
 
 def ultrasonic_movement(destination):
-    servo.run_to_abs_pos(position_sp=destination, speed_sp=75, ramp_down_sp=90)
+    servo.run_to_abs_pos(position_sp=destination, speed_sp=100, ramp_down_sp=90)
 
 
 def scan_walls():
